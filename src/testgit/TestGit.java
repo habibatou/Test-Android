10b330c3418cf4e0ac6d60b3252b0182fd44658f
habibatou <habipenda93@gmail.com>
2018-11-06 15:28:12 +0000

    skt

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -17,6 +17,9 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello Word");
+		System.out.println("modification");
+		
     }
+	
     
 }
